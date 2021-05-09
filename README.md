00dd15f43c6b7acc4ae8248d62f419092522b3c5
Edouard Poitras <eddie@quantumcyberdefence.com>
2021-05-09 14:15:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+﻿# ThemeCreator
+
+An AuroraPatch helper patch primarily used by theme developers to ease the theme creation process.
+
+## Usage
+
+This patch doesn't do anything on its own. It's meant to be used as a dependency of a proper theme patch.
+It exposes a handful of helper methods to adjust Aurora colors.
+
+### public static void AddGlobalColorSwap(Color current, Color swap)
+
+A method used to apply color swaps globally across Aurora.
+Typical use-case would be swapping out the dreaded blue background color throughout the game.
+
+	ThemeCreator.AddGlobalColorSwap(Color.FromArgb(0, 0, 64), Color.Black); // Blue -> Black.
+
+### public static void AddColorChangeByType(Type type, ColorChange colorChange)
+
+A method used to apply background/foreground color changes based on form types.
+Useful to modify all Form/ListView/Button/etc colors.
+
+    // All buttons now have a black background and white text.
+    var colorChange = new ColorChange { BackgroundColor = Color.Black, ForegroundColor = Color.White };
+    ThemeCreator.AddColorChangeByType(typeof(Button), colorChange);
+
+### public static void AddColorChangeByName(string name, ColorChange colorChange)
+    
+Same as above except that it uses the component name instead of a type to apply color changes.
+
+    // The colony toolbar button now has a black background and white text.
+    var colorChange = new ColorChange { BackgroundColor = Color.Black, ForegroundColor = Color.White };
+    ThemeCreator.AddColorChangeByName("cmdToolbarColony, colorChange);
+
+### public static void AddColorChangeByNameRegex(string name, ColorChange colorChange)
+
+Applies a color change if the control name matches the regex provided.
+
+    // All toolbar buttons now have a black background and white text.
+    var colorChange = new ColorChange { BackgroundColor = Color.Black, ForegroundColor = Color.White };
+    ThemeCreator.AddColorChangeByNameRegex(new Regex("cmdToolbar"), colorChange);
+
+## Example
+
+Coming soon.
+
+## Notes
+
+The theme gets applied during the Form.OnShown event.
+
+This means there will be a slight delay between the time a user opens the form initially, and when the color changes actually get applied (a couple seconds on my machine).
+
+## TODO
+
+- Add methods to tweak fonts
+- Add methods to tweak non-control colors, such as map components (like planets, orbits, etc).
---
+++ ThemeCreator.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using HarmonyLib;
+
+namespace ThemeCreator
+{
+    /// <summary>
+    /// Class representing a color change on a Control object.
+    /// </summary>
+    public class ColorChange
+    {
+        public Color BackgroundColor { get; set; }
+        public Color ForegroundColor { get; set; }
+    }
+
+    /// <summary>
+    /// ThemeCreator patch used to facilitate theme creation.
+    /// </summary>
+    public class ThemeCreator : AuroraPatch.Patch
+    {
+        public override string Description => "A helper patch to create theme patches";
+
+        // Global Color Swaps - swap any original Aurora color with another from any control.
+        private static Dictionary<Color, Color> GlobalColorSwaps = new Dictionary<Color, Color>();
+        // Color changes by Type - Set any Type's background and foreground colors.
+        private static Dictionary<Type, ColorChange> TypeColorChanges = new Dictionary<Type, ColorChange>();
+        // Named control color changes.
+        private static Dictionary<string, ColorChange> NameColorChanges = new Dictionary<string, ColorChange>();
+        // Color changes by a regex match of named controls - last resort, not very performant.
+        private static Dictionary<Regex, ColorChange> RegexNameColorChanges = new Dictionary<Regex, ColorChange>();
+
+        /// <summary>
+        /// Initialize our ThemeCreator by patching all Form constructors so that we can inject our colors changes.
+        /// </summary>
+        /// <param name="harmony"></param>
+        protected override void Loaded(Harmony harmony)
+        {
+            LogInfo("Loading ThemeCreator...");
+            HarmonyMethod postfixMethod = new HarmonyMethod(GetType().GetMethod("FormConstructorPostfix", AccessTools.all));
+            foreach (var form in AuroraAssembly.GetTypes().Where(t => typeof(Form).IsAssignableFrom(t)))
+            {
+                try
+                {
+                    foreach (var ctor in form.GetConstructors())
+                    {
+                        harmony.Patch(ctor, postfix: postfixMethod);
+                    }
+                }
+                catch (Exception e)
+                {
+                    LogError($"ThemeCreator failed to patch Form constructor {form.Name}, exception: {e}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Our harmony form constructor postfix method which simply adds our custom shown callback.
+        /// </summary>
+        /// <param name="__instance"></param>
+        private static void FormConstructorPostfix(Form __instance)
+        {
+            __instance.Shown += CustomShown;
+        }
+
+        /// <summary>
+        /// Our custom shown callback is a pass-through method that simply casts our generic callback
+        /// object to a Control and sends it off to the IterateControls method for enumeration/modification.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void CustomShown(Object sender, EventArgs e)
+        {
+            IterateControls((Control)sender);
+        }
+
+        /// <summary>
+        /// Recusively loops through all controls and applies our requested changes.
+        /// </summary>
+        /// <param name="control"></param>
+        private static void IterateControls(Control control)
+        {
+            ApplyChanges(control);
+            foreach (Control ctrl in control.Controls)
+            {
+                IterateControls(ctrl);
+            }
+        }
+
+        /// <summary>
+        /// The meat of our patch. Does all the heavy lifting to apply our color changes.
+        /// </summary>
+        /// <param name="control"></param>
+        private static void ApplyChanges(Control control)
+        {
+            // Global color swaps.
+            if (GlobalColorSwaps.ContainsKey(control.BackColor)) control.BackColor = GlobalColorSwaps[control.BackColor];
+            if (GlobalColorSwaps.ContainsKey(control.ForeColor)) control.ForeColor = GlobalColorSwaps[control.ForeColor];
+            // Type color changes.
+            Type type = control.GetType();
+            if (TypeColorChanges.ContainsKey(type))
+            {
+                var ColorChange = TypeColorChanges[type];
+                control.BackColor = ColorChange.BackgroundColor;
+                control.ForeColor = ColorChange.ForegroundColor;
+            }
+            // Named color changes.
+            if (NameColorChanges.ContainsKey(control.Name))
+            {
+                var ColorChange = NameColorChanges[control.Name];
+                control.BackColor = ColorChange.BackgroundColor;
+                control.ForeColor = ColorChange.ForegroundColor;
+            }
+            // Regex named colors changes.
+            foreach (KeyValuePair<Regex, ColorChange> regexColor in RegexNameColorChanges)
+            {
+                if (regexColor.Key.IsMatch(control.Name))
+                {
+                    control.BackColor = regexColor.Value.BackgroundColor;
+                    control.ForeColor = regexColor.Value.ForegroundColor;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Convert every instance of an original Aurora color into another color.
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="swap"></param>
+        public static void AddGlobalColorSwap(Color current, Color swap)
+        {
+            GlobalColorSwaps[current] = swap;
+        }
+
+        /// <summary>
+        /// Will apply the color changes specified to the type specified.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="colorChange"></param>
+        public static void AddColorChangeByType(Type type, ColorChange colorChange)
+        {
+            TypeColorChanges[type] = colorChange;
+        }
+
+        /// <summary>
+        /// Will apply the color changes specified to the control name specified.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="colorChange"></param>
+        public static void AddColorChangeByName(string name, ColorChange colorChange)
+        {
+            NameColorChanges[name] = colorChange;
+        }
+
+        /// <summary>
+        /// Will apply the color changes specified to the control name matching the regex specified.
+        /// Should be used as a last resort - not very performant.
+        /// </summary>
+        /// <param name="regex"></param>
+        /// <param name="colorChange"></param>
+        public static void AddColorChangeByNameRegex(Regex regex, ColorChange colorChange)
+        {
+            RegexNameColorChanges[regex] = colorChange;
+        }
+    }
+}
